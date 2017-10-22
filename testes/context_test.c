18eced05a1ec1d3ca109ea8b12b714016f442a98
Augusto Boranga <guto.boranga@gmail.com>
2017-10-21 23:41:39 -0200

    atualiza o teste pra cjoin

--- testes/context_test.c
+++ testes/context_test.c
@@ -5,30 +5,23 @@
 csem_t sinaleira;
 
 void* print_the_sound_of_a_cup_hitting_the_ground(void) {
-  printf("\n> creck!\n> Quero entrar na secao critica");
+  printf("\n> creck!");
   
-  cwait(&sinaleira);
-  printf("> SECAO CRITICA NA print_the_sound_of_a_cup_hitting_the_ground\nUHUUL");
-  cyield();
-  csignal(&sinaleira);
-  printf("\n>voltei do csignal e já vou mimbora\n\n");
+  cjoin(1);
+  
+  // deveria vir pra cá só depois de terminar de executar a thread print_the_sound_of_a_capybara
+  printf("> creck de novo, depois de dar cjoin\n\n");
   
   return NULL;
 }
 
 void* print_the_sound_of_a_capybara(void) {
-  printf("> capivara tentando entrar na secao critica");
-  cwait(&sinaleira);
-  printf("\n> SECAO CRITICA!!11!1!!onze!");
   printf("\n\n> AheoUYUOoomTgUU\n\n");
-  csignal(&sinaleira);
   return NULL;
 }
 
 int main() {
   printf("\n\n> começou a main\n\n");
-  
-  csem_init(&sinaleira, 0);
   
   int tid1 = ccreate(&print_the_sound_of_a_capybara, NULL, 0);
   int tid2 = ccreate(&print_the_sound_of_a_cup_hitting_the_ground, NULL, 0);
